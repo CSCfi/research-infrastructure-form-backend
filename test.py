0b96200d2fa3dd1e2941587d328a03878af86a4b
Péter Siket-Szász <peter.siketszasz@gmail.com>
2019-10-07 13:20:36 +0300

    Added script to save form data

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import datetime
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,4 +8,14 @@ def hello():
 
 @app.route("/", methods=['POST'])
 def post_received():
+    date = datetime.datetime.now()
+    with open("/data/www/testserver/formdata/form_" + str(date), "w+") as f:
+        f.write(str(request.form.to_dict()))
+
     return 'thanks'
+
+@app.errorhandler(Exception)
+def exception_handler(error):
+    with open("/data/www/testserver/error", "a+") as f:
+        f.write(repr(error))
+    return 'Error logged'
