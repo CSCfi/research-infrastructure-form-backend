c6e394305861ab6d853e95842b29e8ce1919dacc
Péter Siket-Szász <peter.siket-szasz@csc.fi>
2021-06-15 09:47:12 +0300

    Added function to include lang fields in old forms

--- format_json.py
+++ format_json.py
@@ -52,17 +52,80 @@ def hierarchize(inputdir, form_file, outputdir):
     with open(outputdir + "/" + form_file, "w+", encoding='utf-8') as f:
         f.write(json.dumps(new_dict, ensure_ascii=False, indent=4).encode("utf-8").decode())
 
+
+def add_langs(inputdir, form_file, outputdir):
+    #Load the json
+    with open(inputdir + "/" + form_file, encoding='utf-8') as f:
+        form = json.load(f)
+
+    # Store keys
+    keys = form.keys()
+
+    new_dict = {}
+
+    # Define fields that need langs
+    lang_fields = ['infraDescrSci', 'infraConName', 'infraConDescr', 'infraConInfo', 'infraConTerms', 'CoOrgName']
+    service_lang_fields = ['serName', 'serDescr', 'serDescrsci', 'SerPointName', 'SerPointDescr', 'SerPointInfo', 'SerPointTerms',
+                           'CoOrgOtherName', 'SerPointName_add', 'SerPointDescr_add', 'SerPointInfo_add', 'SerPointTerms_add']
+
+    
+    # Extract common fields
+    common_fields = list(takewhile(lambda x: x != "services", list(keys)))
+    service_fields = list(form['services'][0].keys())
+
+    # Add langs to fields that need it 
+    for field in common_fields:
+        # Add keywordSv separately
+        if field == 'infraKeywords':
+            new_dict[field] = form[field]
+            new_dict['infraKeywordsSv'] = ''
+        if field not in lang_fields:
+            new_dict[field] = form[field]
+        else:
+            new_dict[field + 'Fi'] = form[field]
+            new_dict[field + 'Sv'] = ''
+            new_dict[field + 'En'] = ''
+
+    
+    # Add langs to service fields that need it
+    new_dict['services'] = []
+    for idx, service in enumerate(form['services']):
+        new_dict['services'].append({})
+        for field in service_fields:
+            if field not in service_lang_fields:
+                new_dict['services'][idx][field] = form['services'][idx][field]
+            else:
+                new_dict['services'][idx][field + 'Fi'] = form['services'][idx][field]
+                new_dict['services'][idx][field + 'Sv'] = ''
+                new_dict['services'][idx][field + 'En'] = ''
+
+    # Create output directory if necessary
+    import os
+    try:
+        os.mkdir(outputdir)
+    except FileExistsError:
+        pass
+
+    with open(outputdir + "/" + form_file, "w+", encoding='utf-8') as f:
+        f.write(json.dumps(new_dict, ensure_ascii=False, indent=4).encode("utf-8").decode())
+
 if __name__ == "__main__":
     import sys
     import os
 
     try:
-        inputdir = sys.argv[1]
-        outputdir = sys.argv[2]
+        cmd = sys.argv[1]
+        inputdir = sys.argv[2]
+        outputdir = sys.argv[3]
     except IndexError:
-        print("usage: format_json.py <input directory> <output directory>")
+        print("usage: format_json.py <cmd> <input directory> <output directory>")
         sys.exit(0)
 
-    for filename in os.listdir(inputdir):
-        hierarchize(inputdir, filename, outputdir)
-        
+    if cmd == 'hierarchy':
+        for filename in os.listdir(inputdir):
+            hierarchize(inputdir, filename, outputdir)
+    elif cmd == 'lang':
+        for filename in os.listdir(inputdir):
+            add_langs(inputdir, filename, outputdir)
+    else:
+        raise ValueError('Valid commands: hierarchy, lang. Usage: format_json.py <cmd> <input directory> <output directory>')
